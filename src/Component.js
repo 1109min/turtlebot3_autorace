a21e3de4875990d1d450e0fab647aa4d28748478
1109min <1109min@naver.com>
2023-11-16 23:15:33 +0900

    Fix: Remove Alert

--- src/Component.js
+++ src/Component.js
@@ -7,7 +7,7 @@ function Component(props) {
         <div class="w-full">
             <p class="text-1xl font-bold">{props.mode}</p>
             <div className="bg-white w-full rounded-3xl mt-1 mb-2 ps-6 py-2 shadow-xl transition duration-150 ease-in-out hover:scale-105 hover:bg-sky-200">
-                <CopyToClipboard className="text-sm font-bold whitespace-pre-wrap	" text={props.text} onCopy={() => alert(`"${props.text}"가 복사되었습니다.`)}>
+                <CopyToClipboard className="text-sm font-bold whitespace-pre-wrap	" text={props.text} >
                     <div class="text-sm font-bold whitespace-pre-wrap	">
                         {props.text}
                     </div>
