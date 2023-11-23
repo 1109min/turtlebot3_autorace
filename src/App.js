9da56bfdf84f93e6b7816a2be85500e9882669de
1109min <1109min@naver.com>
2023-11-23 13:17:19 +0900

    Fix: Final

--- src/App.js
+++ src/App.js
@@ -17,17 +17,19 @@ function App() {
           <p class="text-lg font-bold text-red-500">8.2.1 Camera Calibration</p>
           <Component mode="Remote PC" text="roscore" />
           <Component mode="SBC" text="roslaunch turtlebot3_autorace_camera raspberry_pi_camera_publish.launch" />
-          <Component mode="Remote PC" text="rqt_image_view" />
+          <Component mode="SBC" text="roslaunch turtlebot3_bringup turtlebot3_robot.launch" />
+          <Component mode="Remote PC" text="roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch" />
+
         </div>
 
-        <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
+        {/* <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
           <p class="text-lg font-bold text-red-500">8.2.2 Intrinsic Camera Calibration</p>
           <Component mode="Remote PC" text="roscore" />
           <Component mode="SBC" text="roslaunch turtlebot3_autorace_camera raspberry_pi_camera_publish.launch" />
           <Component mode="Remote PC" text="roslaunch turtlebot3_autorace_camera intrinsic_camera_calibration.launch mode:=calibration" />
-        </div>
+        </div> */}
 
-        <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
+        {/* <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
           <p class="text-lg font-bold text-red-500">8.2.3 Extrinsic Camera Calibration</p>
           <Component mode="Remote PC" text="roscore" />
           <Component mode="SBC" text="roslaunch turtlebot3_autorace_camera raspberry_pi_camera_publish.launch" />
@@ -36,33 +38,33 @@ function App() {
           <Component text="rqt" />
           <Component text="rosrun rqt_reconfigure rqt_reconfigure" />
 
-        </div>
+        </div> */}
 
-        <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
+        {/* <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
           <p class="text-lg font-bold text-red-500">8.2.4 Check Calibration Result</p>
           <Component mode="Remote PC" text="roscore" />
           <Component mode="SBC" text="roslaunch turtlebot3_autorace_camera raspberry_pi_camera_publish.launch" />
           <Component mode="Remote PC" text="roslaunch turtlebot3_autorace_camera intrinsic_camera_calibration.launch mode:=action" />
           <Component text="roslaunch turtlebot3_autorace_camera extrinsic_camera_calibration.launch mode:=action" />
           <Component text="rqt" />
-        </div>
+        </div> */}
 
-        <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
+        {/* <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
           <p class="text-lg font-bold text-red-500">8.3 Lane Detection</p>
           <Component mode="Remote PC" text="roscore" />
           <Component mode="SBC" text="roslaunch turtlebot3_autorace_camera raspberry_pi_camera_publish.launch" />
           <Component mode="Remote PC" text="roslaunch turtlebot3_autorace_camera intrinsic_camera_calibration.launch mode:=action" />
           <Component text="roslaunch turtlebot3_autorace_camera extrinsic_camera_calibration.launch mode:=action" />
           <Component text="roslaunch turtlebot3_autorace_detect detect_lane.launch mode:=calibration" />
-        </div>
+        </div> */}
 
-        <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
+        {/* <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
           <p class="text-lg font-bold text-red-500">8.3 Lane Detection With Actual TurtleBot3</p>
           <p> Close rosrun rqt_reconfigure rqt_reconfigure roslaunch turtlebot3_autorace_detect detect_lane.launch mode:=calibration</p>
           <Component mode="Remote PC" text="roslaunch turtlebot3_autorace_detect detect_lane.launch mode:=action" />
           <Component text="roslaunch turtlebot3_autorace_driving turtlebot3_autorace_control_lane.launch" />
           <Component mode="SBC" text="roslaunch turtlebot3_bringup turtlebot3_robot.launch" />
-        </div>
+        </div> */}
 
         <div class="flex flex-wrap bg-blue-200 h-80 mt-4 rounded-3xl px-8 py-4 h-auto w-96" >
           <p class="text-lg font-bold text-red-500">Mission 1 : Intersection</p>
